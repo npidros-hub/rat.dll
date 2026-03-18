9ee40e7993f02c4b321cb59b1f678148ef046364
realzex <npidros@gmail.com>
2026-03-18 10:23:41 +0400

    Create fps.lua

---
+++ fps.lua
@@ -0,0 +1,13 @@
+local vim = game:GetService("VirtualInputManager")
+local camera = workspace.CurrentCamera
+local runService = game:GetService("RunService")
+
+--test
+
+-- getfpscap = function()
+--     local ren = game:GetService("RunService").RenderStepped:Wait()
+--     return math.floor(1 / ren + 0.5)
+-- end
+
+-- setfpscap = function(fps)
+-- end
