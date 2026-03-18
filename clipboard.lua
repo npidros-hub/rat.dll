f890e0c68ee52fb229b2d51e5059c4c514dd005b
realzex <npidros@gmail.com>
2026-03-18 10:27:30 +0400

    Create clipboard.lua

---
+++ clipboard.lua
@@ -0,0 +1,11 @@
+local copy = function(rat)
+  setclipboard(rat)
+end
+
+local setrbxclipboard = function(rat)
+  setclipboard(rat)
+end
+
+local toclipboard = function(rat)
+  setclipboard(rat)
+end
