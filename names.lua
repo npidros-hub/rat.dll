a207437c1c5c512e899b7da7efa426b819195fb3
realzex <npidros@gmail.com>
2026-03-18 10:25:23 +0400

    Create names.lua

---
+++ names.lua
@@ -0,0 +1,16 @@
+-- local GETEXECNAME = 'rat/app'
+-- local version = '1.4.5'
+
+-- local function getexecutorname()
+--     return GETEXECNAME
+-- end
+
+-- local function getexecutorversion()
+--     return version
+-- end
+
+-- local function identifyexecutor()
+--     return getexecutorname()
+-- end
+
+--test
